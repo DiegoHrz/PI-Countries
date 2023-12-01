14781b66477b236e02c4a78dc07166c60c6041f3
DiegoHrz <hernandezwdiego@gmail.com>
2023-11-30 23:54:06 -0500

    Add adjust

--- server/src/handlers/driverHandlers/driverHandler.js
+++ server/src/handlers/driverHandlers/driverHandler.js
@@ -1,10 +1,15 @@
-const {getDrivers, getDriversById, getDriversName, postDrivers} = require('../../controllers/driverController/driverController')
+const {
+  getDrivers,
+  getDriversById,
+  getDriversName,
+  postDrivers,
+} = require("../../controllers/driverController/driverController");
 
 const driverHandler = {};
 
 driverHandler.getDriversHandler = async (req, res) => {
   try {
-    const response = await getDrivers()
+    const response = await getDrivers();
     res.status(200).json(response);
   } catch (error) {
     res.status(404).json({ error: error.message });
@@ -13,8 +18,8 @@ driverHandler.getDriversHandler = async (req, res) => {
 
 driverHandler.getDriversByIdHandler = async (req, res) => {
   try {
-    const {id} = req.params
-    const response = await getDriversById(id)
+    const { id } = req.params;
+    const response = await getDriversById(id);
     res.status(200).json(response);
   } catch (error) {
     res.status(404).json({ error: error.message });
@@ -23,8 +28,8 @@ driverHandler.getDriversByIdHandler = async (req, res) => {
 
 driverHandler.getDriversNameHandler = async (req, res) => {
   try {
-    const {name} = req.query
-    const response = await getDriversName(name)
+    const { name } = req.query;
+    const response = await getDriversName(name);
     res.status(200).json(response);
   } catch (error) {
     res.status(404).json({ error: error.message });
@@ -33,8 +38,8 @@ driverHandler.getDriversNameHandler = async (req, res) => {
 
 driverHandler.postDriversHandler = async (req, res) => {
   try {
-    const {} = req.body
-    const response = await postDrivers()
+    const {} = req.body;
+    const response = await postDrivers();
     res.status(200).json(response);
   } catch (error) {
     res.status(404).json({ error: error.message });
